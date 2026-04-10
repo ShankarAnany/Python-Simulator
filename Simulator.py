4ebf392aeb696b5d6e2cc187cc9ab78884f10d9e
Anany Shankar <shankaranany@gmail.com>
2026-04-10 11:45:24 +0530

    i type jalr, exiting when error occurs

--- Simulator.py
+++ Simulator.py
@@ -126,8 +126,9 @@ def i_type_alu(instr, reg_set):
 
     return reg_set
 
-
 def i_type_lw(instr, reg_set, data_memory, stack_memory):
+    global error
+
     imm = instr[0:12]
     rs_1 = get_field(instr[12:17])
     funct_3 = instr[17:20]
@@ -147,6 +148,7 @@ def i_type_lw(instr, reg_set, data_memory, stack_memory):
 
     if rel_adress % 4 != 0:
         print("Illegal Memory Access", end = "")
+        error = True
         return reg_set
 
     index = rel_adress // 4
@@ -164,9 +166,26 @@ def i_type_lw(instr, reg_set, data_memory, stack_memory):
     return reg_set
 
 def i_type_jalr(instr, reg_set, pc):
+    imm = instr[0:12]
+    rs1 = get_field(instr[12:17])
+    funct3 = instr[17:20]
+    rd = get_field(instr[20:25])
+
+    offset = get_num("0b" + imm, 12)
+    base = get_num(reg_set[rs1])
+
+    if funct3 == "000":
+        addr = get_bin(pc + 4, 32)
+        pc = (base + offset) & ~1
+
+    if rd != 0:
+        reg_set[rd] = addr
+
     return reg_set, pc
 
 def s_type(instr, reg_set, data_memory, stack_memory):
+    global error
+
     imm = instr[0:7] + instr[20:25]
     rs_2 = get_field(instr[7:12])
     rs_1 = get_field(instr[12:17])
@@ -187,6 +206,7 @@ def s_type(instr, reg_set, data_memory, stack_memory):
 
     if rel_adress % 4 != 0:
         print("Illegal Memory Access")
+        error = True
         return data_memory, stack_memory
 
     index = rel_adress // 4
@@ -296,6 +316,8 @@ def main():
     
     # Main Program Loop
     custom_pc_change = False
+    global error
+    error = False
     pc = 0
     while True:
         instr = program_memory[pc // 4]
@@ -324,6 +346,11 @@ def main():
         elif opcode == "1101111":
             reg_set, pc = j_type(instr, reg_set, pc)
             custom_pc_change = True
+        
+        # break if error occurs
+        if error:
+            print(f" in line {pc // 4}")
+            break
 
         if not custom_pc_change:
             pc += 4
