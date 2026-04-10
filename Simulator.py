a4039caa3b7b809a57a2ecedf0552b861c61930e
25254-bit <kartik25254@iiitd.ac.in>
2026-04-10 11:41:39 +0530

    added auipc

--- Simulator.py
+++ Simulator.py
@@ -248,6 +248,17 @@ def u_type_lui(instr, reg_set):
     return reg_set
 
 def u_type_auipc(instr, reg_set, pc):
+    imm = instr[0:20]
+    rd = get_field(instr[20:25])
+
+    opi = get_num(imm, length = 20)
+    offset = opi << 12
+
+    result = get_bin(pc + offset, 32)
+
+    if rd != 0:
+        reg_set[rd] = result
+
     return reg_set
 
 def j_type(instr, reg_set, pc):
